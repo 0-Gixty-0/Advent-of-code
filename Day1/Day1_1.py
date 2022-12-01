eef4c2dfc1a68cbfdce92352d2745a834d120b80
erik-andreasson-nti-johanneberg <Erik.andreasson@elev.ga.ntig.se>
2022-12-01 14:10:14 +0100

    Day1
    Day1 AoC

---
+++ Day1/Day1_1.py
@@ -0,0 +1,35 @@
+
+def format_input_file(path: str) -> list[str]:
+    nums = []
+    elf_iv = []
+    f = open(path, 'r')
+    for data in f:
+        if data == '\n':
+            nums.append(elf_iv)
+            elf_iv = []
+        else:
+            data = data.rstrip('\n')
+            elf_iv.append(data)
+    nums.append(elf_iv)
+    return nums
+        
+
+
+def calculate_highest_total_cal(nums) -> int:
+    total = 0
+    temp = 0
+    for elf in nums:
+        for num in elf:
+            num = int(num)
+            temp += num
+        if temp > total:
+            total = temp
+        temp = 0
+    return total
+
+def main(path) -> int:
+    nums = format_input_file(path)
+    return calculate_highest_total_cal(nums)
+
+print(main("Day1/input.txt"))
+# print(main("Day1/testinput.txt"))
---
+++ Day1/Day1_2.py
@@ -0,0 +1,42 @@
+
+def format_input_file(path: str) -> list[str]:
+    nums = []
+    elf_iv = []
+    f = open(path, 'r')
+    for data in f:
+        if data == '\n':
+            nums.append(elf_iv)
+            elf_iv = []
+        else:
+            data = data.rstrip('\n')
+            elf_iv.append(data)
+    nums.append(elf_iv)
+    return nums
+
+def calculate_totals(nums) -> list[int]:
+    totals = []
+    cal_total = 0
+    for elf in nums:
+        for num in elf:
+            num = int(num)
+            cal_total += num
+        totals.append(cal_total)
+        cal_total = 0
+    return totals
+
+
+
+def calculate_three_highest_total_cal(totals: list[int]):
+    totals.sort()
+    grandtotal = 0
+    for i in range(1,4):
+        grandtotal += totals[-i]
+    return grandtotal
+
+def main(path) -> int:
+    nums = format_input_file(path)
+    totals = calculate_totals(nums)
+    return calculate_three_highest_total_cal(totals)
+
+print(main("Day1/input.txt"))
+# print(main("Day1/testinput.txt"))
